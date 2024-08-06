e94c6516b2d8c6a61035750912a022fa3cce5f5e
asya474 <asya474@gmail.com>
2024-08-07 01:06:02 +0700

    finish version

--- tests/pagination_test.py
+++ tests/pagination_test.py
@@ -24,12 +24,46 @@ def test_users_pagination_minimal_values(app_url):
 
 def test_users_pagination_page_zero(app_url):
     response = requests.get(f"{app_url}/api/users/?page=0&size=1")
-    schema = load_schema("unprocessable_entity.json")
+    schema = load_schema("unprocessable_entity_zero.json")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
     jsonschema.validate(response.json(), schema)
 def test_users_pagination_size_zero(app_url):
     response = requests.get(f"{app_url}/api/users/?page=1&size=0")
-    schema = load_schema("unprocessable_entity.json")
+    schema = load_schema("unprocessable_entity_zero.json")
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
     jsonschema.validate(response.json(), schema)
 
+def test_users_pagination_size_101(app_url):
+    response = requests.get(f"{app_url}/api/users/?page=1&size=101")
+    schema = load_schema("unprocessable_entity_101.json")
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+    jsonschema.validate(response.json(), schema)
+
+def test_users_without_pagination(app_url):
+    response = requests.get(f"{app_url}/api/users/")
+    assert response.status_code == HTTPStatus.OK
+    assert len(response.json()["items"]) == 12
+    assert response.json()["total"] == 12
+    assert response.json()["page"] == 1
+    assert response.json()["size"] == 50
+    assert response.json()["pages"] == 1
+
+@pytest.mark.parametrize("page", ["fafaf"])
+def test_user_page_string(app_url, page):
+    response = requests.get(f"{app_url}/api/users/?page={page}")
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+@pytest.mark.parametrize("size", ["fafaf"])
+def test_user_size_string(app_url, size):
+    response = requests.get(f"{app_url}/api/users/?size={size}")
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+@pytest.mark.parametrize("page, size", [(12, 1), (1, 12), (6, 2), (2, 6)])
+def test_users_successful_pagination(app_url, page, size):
+    response = requests.get(f"{app_url}/api/users/?size={size}&page={page}")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json()["total"] == page * size
+    assert response.json()["page"] == page
+    assert response.json()["size"] == size
+    assert response.json()["pages"] == (page * size) / size
+    assert len(response.json()) == 5
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -6,8 +6,5 @@ import requests
 def test_status_ok(app_url):
     response = requests.get(f"{app_url}/status")
     assert response.status_code == HTTPStatus.OK
+    assert response["users"], "Init data are absent"
 
-
-def test_status_ok(app_url):
-    response = requests.get(f"{app_url}/status").json()
-    assert response["users"]
